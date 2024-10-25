669e1b3d9827cf09b66585ac5e0879fda997828d
Valentyn Sharshon <woosay333@gmail.com>
2024-10-25 12:24:51 +0300

    created ResourceNotFoundException checked exception

---
+++ src/main/java/com/voxloud/provisioning/exception/ResourceNotFoundException.java
@@ -0,0 +1,18 @@
+package com.voxloud.provisioning.exception;
+
+import lombok.Getter;
+
+@Getter
+public class ResourceNotFoundException extends Exception {
+
+    private final String errorPlaceInfo;
+    private final String code;
+    private final String message;
+
+    public ResourceNotFoundException(String errorPlaceInfo, String code, String message) {
+        super(message);
+        this.errorPlaceInfo = errorPlaceInfo;
+        this.code = code;
+        this.message = message;
+    }
+}
